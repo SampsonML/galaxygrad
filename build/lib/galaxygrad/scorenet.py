d0a35b99d21a50969baba282ba079d0197297703
Matt <matt.sampson@princeton.edu>
2024-02-17 18:03:46 -0500

    fixed incorrect Jacobain

--- build/lib/galaxygrad/scorenet.py
+++ build/lib/galaxygrad/scorenet.py
@@ -37,8 +37,8 @@ FN = os.path.join(os.path.dirname(__file__), 'eqx_hsc_ScoreNet64.eqx')
 HSC_64 = eqx.tree_deserialise_leaves(FN, model64)
 def model_wrapper(x, t=0.01):
     sigma = 0.1
-    x = jnp.log(x + 1) / sigma
-    raw_grad = HSC_64(x, t)
+    x_ = jnp.log(x + 1) / sigma
+    raw_grad = HSC_64(x_, t)
     transform_grad = raw_grad * ( 1 / ( sigma * (x + 1) )) # analytic derrivitive    
     return transform_grad
 
@@ -71,8 +71,8 @@ FN = os.path.join(os.path.dirname(__file__), 'eqx_hsc_ScoreNet32.eqx')
 HSC_32 = eqx.tree_deserialise_leaves(FN, model32)
 def model_wrapper(x, t=0.01):
     sigma = 0.1
-    x = jnp.log(x + 1) / sigma
-    raw_grad = HSC_32(x, t)
+    x_ = jnp.log(x + 1) / sigma
+    raw_grad = HSC_32(x_, t)
     transform_grad = raw_grad * ( 1 / ( sigma * (x + 1) )) # analytic derrivitive    
     return transform_grad
 HSC_ScoreNet32_func = model_wrapper
@@ -103,11 +103,11 @@ FN = os.path.join(os.path.dirname(__file__), 'eqx_ZTF_ScoreNet32.eqx')
 ZTF_32 = eqx.tree_deserialise_leaves(FN, model32)
 def model_wrapper(x, t=0.01):
     sigma = 0.1
-    x = jnp.log(x + 1) / sigma
-    raw_grad = ZTF_32(x, t)
+    x_ = jnp.log(x + 1) / sigma
+    raw_grad = ZTF_32(x_, t)
     transform_grad = raw_grad * ( 1 / ( sigma * (x + 1) )) # analytic derrivitive    
     return transform_grad
-ZTF_ScoreNet32 = model_wrapper
+ZTF_ScoreNet32_func = model_wrapper
 
 # define a class to store shape attribute
 class ZTF_Scorenet32:
@@ -135,11 +135,11 @@ FN = os.path.join(os.path.dirname(__file__), 'eqx_ZTF_ScoreNet64.eqx')
 ZTF_64 = eqx.tree_deserialise_leaves(FN, model32)
 def model_wrapper(x, t=0.01):
     sigma = 0.1
-    x = jnp.log(x + 1) / sigma
-    raw_grad = ZTF_64(x, t)
+    x_ = jnp.log(x + 1) / sigma
+    raw_grad = ZTF_64(x_, t)
     transform_grad = raw_grad * ( 1 / ( sigma * (x + 1) )) # analytic derrivitive    
     return transform_grad
-ZTF_ScoreNet64 = model_wrapper
+ZTF_ScoreNet64_fun = model_wrapper
 
 # define a class to store shape attribute
 class ZTF_Scorenet64:
